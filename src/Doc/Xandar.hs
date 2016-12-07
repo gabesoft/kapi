73762e907c633d64b5cadbcc463ce125f0ae24b9
Gabriel Adomnicai <gabesoft@gmail.com>
2016-12-06 22:40:00 -0800

    Handlers - in progress

--- src/Doc/Xandar.hs
+++ src/Doc/Xandar.hs
@@ -1,69 +1,71 @@
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | Documentation generation for Xandar endpoints
-module Doc.Xandar (markdownDoc) where
+module Doc.Xandar
+  ( markdownDoc
+  ) where
 
+import Api.Xandar
+import Data.Proxy
 import Data.Text (Text)
 import Servant.API
 import Servant.Docs
-import Data.Proxy
 import Types.Xandar
-import Api.Xandar
 
 instance ToSample User where
-    toSamples _ = singleSample u1
+  toSamples _ = singleSample u1
 
 instance ToSample Error where
-    toSamples _ = singleSample (Error "something bad happened")
+  toSamples _ = singleSample (Error "something bad happened")
 
 instance ToSample (ModelOrError User) where
-    toSamples _ = singleSample (Succ u1)
+  toSamples _ = singleSample (Succ u1)
 
 instance ToCapture (Capture "id" Text) where
-    toCapture _ = DocCapture "id" "user identifier"
+  toCapture _ = DocCapture "id" "user identifier"
 
 instance ToParam (QueryParams "include" String) where
-    toParam _ =
-        DocQueryParam
-            "include"
-            ["name", "profile.description"]
-            "a list of fields to be included in the objects returned"
-            List
+  toParam _ =
+    DocQueryParam
+      "include"
+      ["name", "profile.description"]
+      "a list of fields to be included in the objects returned"
+      List
 
 instance ToParam (QueryParam "where" String) where
-    toParam _ =
-        DocQueryParam
-            "where"
-            ["(name eq 'Sherlock') or (profile.date lt '12999888')"]
-            "record filters"
-            Normal
+  toParam _ =
+    DocQueryParam
+      "where"
+      ["(name eq 'Sherlock') or (profile.date lt '12999888')"]
+      "record filters"
+      Normal
 
 instance ToParam (QueryParams "sort" String) where
-    toParam _ =
-        DocQueryParam
-            "sort"
-            ["+name", "-createdAt"]
-            "sort fields +ascending or -descending"
-            List
+  toParam _ =
+    DocQueryParam
+      "sort"
+      ["+name", "-createdAt"]
+      "sort fields +ascending or -descending"
+      List
 
 instance ToParam (QueryParam "start" Int) where
-    toParam _ = DocQueryParam "start" ["0"] "start index for pagination" Normal
+  toParam _ = DocQueryParam "start" ["0"] "start index for pagination" Normal
 
 instance ToParam (QueryParam "limit" Int) where
-    toParam _ =
-        DocQueryParam
-            "limit"
-            ["50"]
-            "the number of records to return (page size)"
-            Normal
+  toParam _ =
+    DocQueryParam
+      "limit"
+      ["50"]
+      "the number of records to return (page size)"
+      Normal
 
 instance ToSample Char where
-    toSamples _ = singleSample ' '
+  toSamples _ = singleSample ' '
 
 instance ToSample Int where
-    toSamples _ = singleSample 0
+  toSamples _ = singleSample 0
 
 markdownDoc :: String
 markdownDoc = markdown $ docs (Proxy :: Proxy XandarApi)
---
+++ src/Handlers/Users/Xandar.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Handlers for xandar endpoints
+module Handlers.Users.Xandar where
+
+import Api.Xandar
+import Data.Text (Text)
+import GHC.TypeLits
+import Servant
+import Servant.API
+import Types.Xandar
+
+server :: Server XandarApi
+server =
+  getMultiple :<|> getSingle :<|> deleteSingle :<|> createSingle :<|>
+  createMultiple :<|>
+  replaceSingle :<|>
+  replaceMultiple :<|>
+  modifySingle :<|>
+  modifyMultiple :<|>
+  headSingle :<|>
+  headMultiple :<|>
+  optionsSingle :<|>
+  optionsMultiple
+
+-- getMultiple
+--   :: (KnownSymbol a, KnownSymbol b)
+--   => String
+--   -> String
+--   -> String
+--   -> String
+--   -> String
+--   -> String
+--   -> Handler (Headers '[Header a String, Header b String] [User])
+getMultiple fields query sort start limit =
+  return $ addHeader "abcd" (addHeader "10" [u1, u2])
+
+getSingle :: Text -> Handler User
+getSingle uid =
+  if uid == "1"
+    then return u1
+    else throwError $
+         err404 {errBody = "A user matching the input id was not found"}
+
+deleteSingle = undefined
+
+createSingle = undefined
+
+createMultiple = undefined
+
+replaceSingle = undefined
+
+replaceMultiple = undefined
+
+modifySingle = undefined
+
+modifyMultiple = undefined
+
+headSingle = undefined
+
+headMultiple = undefined
+
+optionsSingle = undefined
+
+optionsMultiple = undefined
