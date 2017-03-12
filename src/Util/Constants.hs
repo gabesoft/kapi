a1cd078dd096a1dfcd1d6edf94818190c1449e59
Gabriel Adomnicai <gabesoft@gmail.com>
2017-03-12 10:29:07 -0700

    Comments

--- src/Util/Constants.hs
+++ src/Util/Constants.hs
@@ -17,5 +17,9 @@ idLabel = "_id"
 
 -- ^
 -- Maximum number of results returned from a search or query
+-- For elastic search the default for an index is 10000
+-- The index max_result_window value must be increased to allow for
+-- higher values
+-- curl -XPUT "http://localhost:9200/kapi-xandar/_settings" -d '{ "index" : { "max_result_window" : 500000 } }'
 maxResultsSize :: Int
 maxResultsSize = 10000
